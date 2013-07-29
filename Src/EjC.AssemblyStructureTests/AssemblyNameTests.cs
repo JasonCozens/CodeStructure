90e80157937a3e164d321da49719d09da2ed03cf
Jason Cozens <jason.cozens@computer.org>
2013-07-29 23:31:48 +0100

    Tests around framework class AssemblyName
    I needed to write some tests as the AssemblyName class can't be faked.

---
+++ Src/EjC.AssemblyStructureTests/AssemblyNameTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration.Assemblies;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace EjC.AssemblyStructureTests
+{
+    /// <summary>
+    /// It is not possible for Fakes to create a Stub or Shim for
+    /// <see cref="System.Reflection.AssemblyName"/>.
+    /// </summary>
+    [TestClass]
+    public class AssemblyNameTests
+    {
+        [TestMethod]
+        public void NewAssemblyName_InitialPropertyValues()
+        {
+            var assemblyName = new AssemblyName();
+            // Assert
+            Assert.AreEqual(null, assemblyName.CodeBase);
+            Assert.AreEqual(AssemblyContentType.Default, assemblyName.ContentType);
+            Assert.AreEqual(null, assemblyName.CultureInfo);
+            Assert.AreEqual(null, assemblyName.CultureName);
+            Assert.AreEqual(null, assemblyName.EscapedCodeBase);
+            Assert.AreEqual(AssemblyNameFlags.None, assemblyName.Flags);
+            Assert.AreEqual(string.Empty, assemblyName.FullName);
+            Assert.AreEqual(AssemblyHashAlgorithm.None, assemblyName.HashAlgorithm);
+            Assert.AreEqual(null, assemblyName.KeyPair);
+            Assert.AreEqual(null, assemblyName.Name);
+            Assert.AreEqual(ProcessorArchitecture.None, assemblyName.ProcessorArchitecture);
+            Assert.AreEqual(null, assemblyName.Version);
+            Assert.AreEqual(AssemblyVersionCompatibility.SameMachine, assemblyName.VersionCompatibility);
+        }
+    }
+}
