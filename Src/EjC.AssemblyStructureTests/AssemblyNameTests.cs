a173e579e514ed1ef1a4e78a21bc6c5268f80078
Jason Cozens <jason.cozens@computer.org>
2013-07-29 23:55:34 +0100

    Added AssemblyName tests for constructor that takes a full name.

--- Src/EjC.AssemblyStructureTests/AssemblyNameTests.cs
+++ Src/EjC.AssemblyStructureTests/AssemblyNameTests.cs
@@ -13,6 +13,9 @@ namespace EjC.AssemblyStructureTests
     [TestClass]
     public class AssemblyNameTests
     {
+        /// <summary>
+        /// Property values for AssemblyName using the default constructor.
+        /// </summary>
         [TestMethod]
         public void NewAssemblyName_InitialPropertyValues()
         {
@@ -30,6 +33,35 @@ namespace EjC.AssemblyStructureTests
             Assert.AreEqual(null, assemblyName.Name);
             Assert.AreEqual(ProcessorArchitecture.None, assemblyName.ProcessorArchitecture);
             Assert.AreEqual(null, assemblyName.Version);
+            Assert.AreEqual(AssemblyVersionCompatibility.SameMachine, assemblyName.VersionCompatibility);
+        }
+
+        /// <summary>
+        /// Property values for AssemblyName when an assembly string is used.
+        /// </summary>
+        [TestMethod]
+        public void NewAssemblyName_WithDsiplayName_InitialPropertyValues()
+        {
+            // Arrange
+            var name = "EjC.AssemblyStructureTests";
+            var versionString = "1.0.0.0";
+            var version = new Version(versionString);
+            var fullName = string.Format("{0}, Version={1}, Culture=neutral, PublicKeyToken=null", name, versionString);
+            // Act
+            var assemblyName = new AssemblyName(fullName);
+            // Assert
+            Assert.AreEqual(null, assemblyName.CodeBase);
+            Assert.AreEqual(AssemblyContentType.Default, assemblyName.ContentType);
+            Assert.AreEqual(CultureInfo.InvariantCulture, assemblyName.CultureInfo);
+            Assert.AreEqual(string.Empty, assemblyName.CultureName);
+            Assert.AreEqual(null, assemblyName.EscapedCodeBase);
+            Assert.AreEqual(AssemblyNameFlags.None, assemblyName.Flags);
+            Assert.AreEqual(fullName, assemblyName.FullName);
+            Assert.AreEqual(AssemblyHashAlgorithm.None, assemblyName.HashAlgorithm);
+            Assert.AreEqual(null, assemblyName.KeyPair);
+            Assert.AreEqual(name, assemblyName.Name);
+            Assert.AreEqual(ProcessorArchitecture.None, assemblyName.ProcessorArchitecture);
+            Assert.AreEqual(version, assemblyName.Version);
             Assert.AreEqual(AssemblyVersionCompatibility.SameMachine, assemblyName.VersionCompatibility);
         }
     }
