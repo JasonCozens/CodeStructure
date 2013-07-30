7ddb7b6190d3a7c40d26749b0b24433b49ebdbc1
Jason Cozens <jason.cozens@computer.org>
2013-07-30 08:23:08 +0100

    Extract the assembly names from an assembly.

--- Src/EjC.AssemblyStructureTests/AssemblyDependenciesTests.cs
+++ Src/EjC.AssemblyStructureTests/AssemblyDependenciesTests.cs
@@ -45,5 +45,33 @@ namespace EjC.AssemblyStructureTests
             Assert.AreEqual(1, dependencies.Count());
             Assert.AreEqual(name, dependencies[0]);
         }
+
+        [TestMethod]
+        public void Analyse_AssemblyWithMultipleReferences_HasOneDirectDependency()
+        {
+            // Arrange
+            var name01 = "EjC.Tests.Assembly01";
+            var name02 = "EjC.Tests.Assembly02";
+            var name03 = "EjC.Tests.Assembly03";
+            var refencedassembly01 = new AssemblyName(name01 + ", Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            var refencedassembly02 = new AssemblyName(name02 + ", Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            var refencedassembly03 = new AssemblyName(name03 + ", Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            Assembly assembly = new StubAssembly()
+            {
+                GetReferencedAssemblies01 = () => new AssemblyName[] {
+                    refencedassembly01,
+                    refencedassembly02,
+                    refencedassembly03}
+            };
+            IAssemblyDependencies assemblyDepencies = new AssemblyDependencies();
+            // Act
+            assemblyDepencies.Analyse(assembly);
+            var dependencies = assemblyDepencies.DirectDependencies.ToArray();
+            // Assert
+            Assert.AreEqual(3, dependencies.Count());
+            Assert.IsTrue(dependencies.Contains(name01));
+            Assert.IsTrue(dependencies.Contains(name02));
+            Assert.IsTrue(dependencies.Contains(name03));
+        }
     }
 }
