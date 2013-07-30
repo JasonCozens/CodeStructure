0c2ae70584e6d3df72e6a702cd4e0597148168a9
Jason Cozens <jason.cozens@computer.org>
2013-07-30 13:43:42 +0100

    New Graph implemented.

---
+++ Src/EjC.AssemblyStructure/Graph.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjC.AssemblyStructure
+{
+    public class Graph<T> : IGraph<T>
+    {
+        ConcurrentDictionary<T, T> _graph;
+
+        public Graph()
+        {
+            _graph = new ConcurrentDictionary<T, T>();
+        }
+
+        public void AddVertices(T parent, IEnumerable<T> children)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Nodes
+        {
+            get { return _graph.Keys; }
+        }
+
+        public IEnumerable<IVertex<T>> Vertices
+        {
+            get { return _graph.Select(v => new Vertex<T>(v.Key, v.Value)); }
+        }
+    }
+}
---
+++ Src/EjC.AssemblyStructureTests/GraphTests.cs
@@ -0,0 +1,24 @@
+﻿using EjC.AssemblyStructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjC.AssemblyStructureTests
+{
+    [TestClass]
+    public class GraphTests
+    {
+        [TestMethod]
+        public void NewGraph_IsEmpty()
+        {
+            // Act
+            IGraph<string> graph = new Graph<string>();
+            // Assert
+            Assert.AreEqual(0, graph.Nodes.Count());
+            Assert.AreEqual(0, graph.Vertices.Count());
+        }
+    }
+}
