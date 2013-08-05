004d587299ad54fdc797e29b158c27047eb0825e
Jason Cozens <jason.cozens@computer.org>
2013-08-05 17:20:49 +0100

    Test of concurrent bag.
    This shows its a bag not a set or list.

---
+++ Src/EjC.AssemblyStructureTests/ConcurrentBagTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EjC.AssemblyStructureTests
+{
+    [TestClass]
+    public class ConcurrentBagTests
+    {
+        /// <summary>
+        /// This is a Bag not a Set.
+        /// </summary>
+        [TestMethod]
+        public void AddItemTwice()
+        {
+            var bag = new ConcurrentBag<string>();
+            bag.Add("TEST");
+            bag.Add("TEST");
+            Assert.AreEqual(2, bag.Count);
+        }
+    }
+}
